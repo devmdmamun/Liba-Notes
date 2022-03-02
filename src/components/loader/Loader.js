0c0667b8c72e4187085c6ab6c0f98b85f5ff81c3
Md Mamun Islam <md.mamun.islam.santo@gmail.com>
2022-03-02 20:53:36 +0600

    created a loading animetion

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,8 @@
+import classes from "./Loader.module.css";
+export const Loader = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.loader}></div>
+    </div>
+  );
+};

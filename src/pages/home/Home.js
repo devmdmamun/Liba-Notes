eef9dd9411ec5605fd65d9db0203c7a158ca6f06
Md Mamun Islam <md.mamun.islam.santo@gmail.com>
2022-03-06 22:35:48 +0600

    updated info

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import classes from "./Home.module.css";
 
 import { useSelector } from "react-redux";
 import { selectUser } from "../../app/features/userSlice";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   const user = useSelector(selectUser);
@@ -12,6 +13,23 @@ export const Home = () => {
         We're glad that you joined our community. As you can see, we are still
         working on this project.Please be patient, we will add all the features
         soon. We are creating this project for educational purposes.
+      </p>
+      <h3 className={classes.h3}>Current status {"(*> _ <*)"} </h3>
+      <p className={classes.info}>
+        Currently we only have{" "}
+        <Link className={classes.link} to="todo">
+          TODO
+        </Link>{" "}
+        feature. So go check it out. If you wanna logout, go to settings then
+        click the sign out button.{" "}
+        <a
+          href="https://blog.devmdmamun.com/why-would-my-first-startup-fail-and-why-im-not-seeing-it-as-a-failure"
+          target="_blank"
+          rel="noreferrer"
+          className={classes.link}
+        >
+          Learn more about this project.
+        </a>
       </p>
     </div>
   );

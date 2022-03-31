bce1816b914594dc653fb9024dd1c28dac977a53
Md Mamun Islam <md.mamun.islam.santo@gmail.com>
2022-03-31 22:26:44 +0600

    changed the profile UI

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,11 +1,26 @@
+// style
 import classes from "./Profile.module.css";
+
+// packages
 import { NavLink } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../app/features/userSlice";
+
+// components and assets
 import { Post } from "../../components/post/Post";
 import { ProfileHead } from "./ProfileHead";
 import { ReactComponent as Filter } from "../../assets/icons/filter.svg";
 import { ReactComponent as Pen } from "../../assets/icons/pen.svg";
 
 export const Profile = () => {
+  const { id } = useParams();
+  const user = useSelector(selectUser);
+
+  if (id !== user.uid) {
+    return <p>Don't have access</p>;
+  }
+
   return (
     <div className={classes.profileCont}>
       <div className={classes.profileHead}>
@@ -19,11 +34,11 @@ export const Profile = () => {
       <div className={classes.profileBottom}>
         <div className={classes.postTop}>
           <div className={classes.createPost}>
-            <Pen className={classes.PTIcon} /> <span>Create post</span>
+            <Pen className={classes.PTIcon} /> <span>Start a post</span>
           </div>
           <div className={classes.sortPost}>
             <Filter className={classes.PTIcon} />
-            <span>Sort Post</span>
+            <span>Sort posts</span>
           </div>
         </div>
         <div className={classes.post}>
--- src/pages/profile/ProfileHead.js
+++ src/pages/profile/ProfileHead.js
@@ -1,28 +1,32 @@
+// Styles
 import classes from "./ProfileHead.module.css";
 
+// assets
+import { ReactComponent as Clink } from "../../assets/icons/link.svg";
+
+// packages and hooks
 import { useSelector } from "react-redux";
 import { selectUser } from "../../app/features/userSlice";
-import banner from "../../assets/soaligFounder.png";
-import { ReactComponent as Clink } from "../../assets/icons/link.svg";
 
 export const ProfileHead = () => {
   const user = useSelector(selectUser);
   return (
-    <>
-      <div className={classes.banner}>
-        <img src={banner} alt="banner" />
-      </div>
-      <img className={classes.profileImg} src={user.photoURL} alt="Profile" />
+    <div className={classes.profileHeadDiv}>
       <div className={classes.bottomHeader}>
-        <p className={classes.name}>{user.displayName}</p>
-        <span>{user.uid}</span>
-        <p className={classes.bio}>
-          Founder of Soalig | web developer | Creating valuable products |
-          Sharing my journey on twitter, soalig & my blog :
-          https://blog.devmdmamun.com
-        </p>
+        <div className={classes.hTop}>
+          <img
+            className={classes.profileImg}
+            src={user.photoURL}
+            alt="Profile"
+          />
+          <div>
+            <p className={classes.name}>{user.displayName}</p>
+            <span>{user.uid}</span>
+          </div>
+        </div>
+        <p className={classes.bio}>{user.bio}</p>
         <div className={classes.links}>
-          <Clink className={classes.Clink} />{" "}
+          <Clink className={classes.Clink} />
           <a
             className={classes.webLink}
             href="https://devmdmamun.com"
@@ -32,11 +36,7 @@ export const ProfileHead = () => {
             DevMdMamun.com
           </a>
         </div>
-        <div className={classes.num}>
-          <span>100 connections</span>
-          <span>7 contracts</span>
-        </div>
       </div>
-    </>
+    </div>
   );
 };

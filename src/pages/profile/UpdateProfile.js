92dce6b14102bd607c295a8a5cb69539bc9dffb0
Md Mamun Islam <md.mamun.islam.santo@gmail.com>
2022-04-16 22:00:49 +0600

    added functional backdrop

--- src/pages/profile/UpdateProfile.js
+++ src/pages/profile/UpdateProfile.js
@@ -3,33 +3,58 @@ import classes from "./UpdateProfile.module.css";
 
 // packages and hooks
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../app/features/userSlice";
 
 export const UpdateProfile = () => {
   const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const [name, setName] = useState(user.displayName);
+  const [website, setWebsite] = useState(user.pLink);
+  const [bio, setBio] = useState(user.bio);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Updated");
+    navigate(-1);
+  };
+
   return (
     <div className={classes.updateProfileCont}>
-      <div className={classes.updateProfile}>
+      <div className={classes.backdrop} onClick={() => navigate(-1)} />
+      <form onSubmit={handleSubmit} className={classes.updateProfile}>
         <div className={classes.updateProfileHeader}>
           <h3>Edit profile</h3>
-          <button onClick={() => navigate(-1)} className={classes.btn}>
-            Save
-          </button>
+          <button className={classes.btn}>Save</button>
         </div>
         <div className={classes.updateProfileContent}>
-          <div className={classes.UPInputDiv}>
+          <label className={classes.UPInputDiv}>
             <span>Name:</span>
-            <input autoFocus type="text"></input>
-          </div>
-          <div className={classes.UPInputDiv}>
+            <input
+              autoFocus
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            ></input>
+          </label>
+          <label className={classes.UPInputDiv}>
             <span>Website:</span>
-            <input type="url"></input>
-          </div>
-          <div className={classes.UPInputDiv}>
+            <input
+              type="url"
+              onChange={(e) => setWebsite(e.target.value)}
+              value={website}
+            ></input>
+          </label>
+          <label className={classes.UPInputDiv}>
             <span>Bio:</span>
-            <textarea></textarea>
-          </div>
+            <textarea
+              onChange={(e) => setBio(e.target.value)}
+              value={bio}
+            ></textarea>
+          </label>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
